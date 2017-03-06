af5e9dde4fc97d2ea9b5c4ac362d42d282c61f9f
rhinoleo <renaud.denier@gmail.com>
2017-03-06 16:46:14 +0100

    First commit

---
+++ carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		autoplay: true,
+		arrows: true,
+		autoplaySpeed: 3000
+  });
+});
